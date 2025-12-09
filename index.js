82f1cabe593cec4e69f384f83d0d39f4f4051112
FakeUser <fake@example.com>
2025-12-09 17:18:43 +0100

    Dodanie kluczy Supabase (service_role i anon) w kodzie JavaScript

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // Źródło z wyciekiem
 const secretToken = "tokentest123";
 
+// Supabase service_role key w kodzie źródłowym
+const supabaseServiceRole = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRlc3QiLCJyb2xlIjoic2VydmljZV9yb2xlIn0.dummy";
+const supabaseAnon = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRlc3QiLCJyb2xlIjoiYW5vbiJ9.dummy";
+
 console.log("Loaded JS file");
 
